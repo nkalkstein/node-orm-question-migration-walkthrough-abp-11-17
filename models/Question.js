5a48f8eaeae7b0d9a1e3d2ff4614a51c4ded5716
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-23 01:11:44 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -1,17 +1,8 @@
 'use strict';
 
+const db 
 const db = require("../config/db")
-
-class Question{
-
-}
-
-
-module.exports = Question;
-
-
-const db = require("../config/db"
-
+ 
 class Question{
   static CreateTable() {
     return new Promise(function(resolve){
@@ -19,12 +10,12 @@ class Question{
         id INTEGER PRIMARY KEY,
         content TEXT
       )`
-
+ 
       db.run(sql, function(){
         resolve("questions table created")
-      })
+      })      
     })
   }
 }
-
+ 
 module.exports = Question;
